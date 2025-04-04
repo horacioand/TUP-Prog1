898ffbfa0326aa836d6bc5d2aa332f843777d1f1
Horacio <horacioanadon00@gmail.com>
2025-04-03 21:28:54 -0300

    carga de unidad 3. ejercicio 1

---
+++ Unidad 3/Ejercicio1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/*Hacer un programa para mostrar por pantalla los números del 1 al 10.
+Importante: El programa no tiene ningún ingreso de datos.
+*/
+
+int main(){
+    for(int i=0; i<10; i++){
+        cout<<i+1<<endl;
+    }
+}
