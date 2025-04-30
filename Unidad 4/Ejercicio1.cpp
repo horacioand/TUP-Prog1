2f62982edf74280bb0be2bb07d0a4db8fb0226a5
horacioand <horacioanadon00@gmail.com>
2025-04-29 21:03:46 -0300

    comienzo unidad 4

---
+++ Unidad 4/Ejercicio1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/*
+Hacer un programa para mostrar por pantalla los números del 1 al 10.
+Importante: El programa no tiene ningún ingreso de datos.
+*/
+int main(){
+    int num=1;
+    while(num<=10){
+        cout<<num<<endl;
+        num++;
+    }
+    return 0;
+}
