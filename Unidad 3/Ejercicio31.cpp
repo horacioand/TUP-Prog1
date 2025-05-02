b60bb8025216bfb33628461f029274193d960288
Horacio <horacioanadon00@gmail.com>
2025-05-02 14:37:57 -0300

    31

--- Unidad 3/Ejercicio31.cpp
+++ Unidad 3/Ejercicio31.cpp
@@ -10,12 +10,56 @@ Ejemplo C: 20, 20, 12, 9, 14, 14, 8, 8, 10, 3 el resultado será 20 y 14.
 En el ejemplo C el valor 20 aparece dos veces, pero sólo se considera una vez.
 */
 
-int main(){
+int main()
+{
     int numero, maximo, segundoMaximo;
-    bool segundoMax;
-    for(int i=0; i<10; i++){
+    bool hayMax=false, haySegundoMax=false;
+    for(int i=0; i<10; i++)
+    {
         cout<<"Ingrese un numero:";
         cin>>numero;
+        //si no hay maximo
+        if(!hayMax)
+        {
+            maximo=numero;
+            hayMax=true;
+        }
+        //si hay maximo
+        else
+        {
+            if(numero==maximo|| (haySegundoMax&&numero==segundoMaximo)){
+                continue;
+            }
+            //si no hay segundo maximo y el numero es menor al maximo
+            if(!haySegundoMax && numero<maximo)
+            {
+                segundoMaximo=numero;
+                haySegundoMax=true;
+            }
+            //si no hay segundo maximo y el numero es mayor al maximo
+            else if(!haySegundoMax && numero>maximo)
+            {
+                segundoMaximo=maximo;
+                maximo=numero;
+                haySegundoMax=true;
+            }
+            //si hay segundo maximo
+            else
+            {
+                //si el numero es mayor al maximo
+                if(numero>maximo)
+                {
+                    segundoMaximo=maximo;
+                    maximo=numero;
+                }
+                //si el numero es menor al maximo y mayor al segundo maximo
+                else if(numero<maximo && numero > segundoMaximo)
+                {
+                    segundoMaximo=numero;
+                }
+                //si el numero es menor a ambos no se realizan acciones
+            }
+        }
 
     }
     cout<<"Maximo: "<<maximo<<endl;
