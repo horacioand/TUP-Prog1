48bff25f7802a99f7587e65684372fe387c53a7a
Horacio <horacioanadon00@gmail.com>
2025-05-08 23:31:07 -0300

    e24

---
+++ Unidad 4/Ejercicio24.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/*
+ Se dispone de la información de los exámenes rendidos por algunos
+estudiantes de la UTN FRGP. Por cada registro de examen se conoce:
+- Legajo del estudiante (entero)
+- Código de materia (entero)
+- Nota (float)
+La finalización de la carga de datos se indica con un legajo de estudiante mayor
+a 30000. Calcular e informar:
+- La nota promedio entre todos los estudiantes.
+- El legajo del estudiante con menor nota.
+- La cantidad de exámenes rendidos para la materia 10.
+- El porcentaje de aprobados y no aprobados.
+NOTA: Un examen se considera aprobado con nota >= 6
+NOTA: Si hay varios estudiantes con la menor nota. Informar el primero de ellos.
+*/
+
+int main(){
+
+
+
+    return 0;
+}
