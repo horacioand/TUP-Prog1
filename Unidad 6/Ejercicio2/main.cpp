d183d8c58b7e7e6fb02140065e8f6d5795486379
Horacio <horacioanadon00@gmail.com>
2025-05-11 19:27:18 -0300

    e2

---
+++ Unidad 6/Ejercicio2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/*
+Hacer una función llamada CalcularMaximo que determine el máximo entre
+dos números. La función debe recibir dos números enteros por valor y devolver
+el valor más grande. Si los números son iguales debe devolver cualquiera de
+los dos. La función no debe mostrar nada por pantalla.
+Hacer un programa para ingresar dos números y, utilizando CalcularMaximo,
+emita luego el número mayor de ambos.
+*/
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
