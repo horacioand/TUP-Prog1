ccc0b0381dccf35933700f3eb0d166cd9d2cb929
Horacio <horacioanadon00@gmail.com>
2025-05-10 23:49:40 -0300

    e24

--- Unidad 4/Ejercicio24.cpp
+++ Unidad 4/Ejercicio24.cpp
@@ -16,9 +16,61 @@ NOTA: Un examen se considera aprobado con nota >= 6
 NOTA: Si hay varios estudiantes con la menor nota. Informar el primero de ellos.
 */
 
-int main(){
+int main()
+{
+    int conExamenes=0, conMateria10=0, conAprobados=0, legajo, codMateria,  legajoMenorNota, menorNota;
+    float nota, notaPromedio, porcAprobados, porcDesaprobados, acuPromedio=0;
+    bool hayMenor = false;
+    cout<<"Ingrese el legajo: "; cin>> legajo;
 
+    while(legajo<=30000)
+    {
+        cout<<"Ingrese el codigo de materia: "; cin>> codMateria;
+        cout<<"Ingrese la nota: "; cin>> nota;
 
+        //contador materia 10
+        if(codMateria==10){
+            conMateria10++;
+        }
+
+        //menor nota
+        if(!hayMenor)
+        {
+            menorNota=nota;
+            legajoMenorNota=legajo;
+            hayMenor=true;
+        }
+        else
+        {
+            if(nota<menorNota)
+            {
+                menorNota=nota;
+                legajoMenorNota=legajo;
+            }
+        }
+
+        //nota promedio
+        conExamenes++;
+        acuPromedio += nota;
+
+        //porcentaje aprobacion
+        if(nota>=6){
+            conAprobados++;
+        }
+
+        cout<<"Ingrese el legajo: "; cin>> legajo;
+    }
+    //porcentaje aprobacion
+    porcAprobados=float(conAprobados*100)/conExamenes;
+    porcDesaprobados=(float)100-porcAprobados;
+    cout<<"Porcentaje de alumnos aprobados: "<<porcAprobados<<" .Porcentaje de alumnos desaprobados: "<<porcDesaprobados<<endl;
+    //nota promedio
+    notaPromedio = (float)acuPromedio/conExamenes;
+    cout<<"La nota promedio es "<< notaPromedio<<endl;
+    //menor nota
+    cout<<"La nota mas baja es "<<menorNota<<" del legajo "<<legajoMenorNota<<endl;
+    //materia 10
+    cout<<"La cantidad de examenes rendidos para la materia 10 es de: "<<conMateria10<<endl;
 
     return 0;
 }
